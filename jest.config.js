71f5341bcab766f78354b1bc1336c2a5a18090e0
prokosna <prokosna@gmail.com>
2025-01-23 23:28:09 +0900

    wip: setup typescript environment

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,12 @@
+/** @type {import("ts-jest/dist/types").JestConfigWithTsJest} */
 module.exports = {
+  preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: [
-    '**/__tests__/**/*.test.js'
+    '**/__tests__/**/*.test.ts'
   ],
   collectCoverageFrom: [
-    'lib/**/*.js'
+    'lib/**/*.ts'
   ],
   coverageDirectory: 'coverage',
   coverageReporters: ['text', 'lcov', 'clover'],
